62e4db8ff729cc1b77027245d456461784ca7767
Jivan517 <np.jiv004@gmail.com>
2016-04-27 13:22:44 -0500

    added solutions for lab1

---
+++ Lab1/code/src/cs544/algorithm/GCD.java
@@ -0,0 +1,19 @@
+package cs544.algorithm;
+
+public class GCD {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("GCD of 8, 12: " + gcd(8,12));
+
+		System.out.println("GCD of 24, 15: " + gcd(24,15));
+	}
+	
+	private static int gcd(int m, int n){
+		if(n == 0)
+			return m;
+		return gcd(n, m%n);
+	}
+
+}
