5756c63a10f0f05548d28338bb78dddb5ea94a32
Jivan517 <np.jiv004@gmail.com>
2016-05-01 15:21:01 -0500

    bubble sort simple added

--- Practice/src/practice/algorithms/BubbleSort.java
+++ Practice/src/practice/algorithms/BubbleSort.java
@@ -14,6 +14,21 @@ public class BubbleSort {
 
 	}
 
+	//Certainly I(0) is true (when
+	/*
+	 * i=0 pass completes, largest element has been placed at the end). Assume
+	 * I(i) holds; we show arr[n-i-2]..arr[n-1] are in final sorted order. As
+	 * inner loop runs, the largest element max among the elements arr[0],
+	 * arr[1],..., arr[n-i-2] is pushed to the right as far as possible. Since
+	 * the elements arr[n-i-1] through arr[n-1] are in final sorted order, max
+	 * is not pushed into any of those final slots. Therefore, it is placed in
+	 * slot n-i-2.
+	 */
+	/**
+	 * 
+	 * @param arr
+	 * @return
+	 */
 	private static int[] bubbleSort(int[] arr) {
 
 		for(int i = 0; i < arr.length; i++){
